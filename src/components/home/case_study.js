e01f875c05cd71e83e0a12dd59a19ac30943c72a
chwebdev815 <xandokan815@gmail.com>
2020-04-17 21:45:51 +0500

    files deleted

--- src/components/home/case_study.js
+++ src/components/home/case_study.js
@@ -1,25 +1,26 @@
 import React from "react"
 
-
-export default function  Case_study({
-    title,
-    button_text,
-    description,
-    boldtext
-
-}){
-
-return(
-        
-<div className="Case_study">
-<div className="content">
-<h3>{title}</h3>
-<a  href="Anatomie-Metrical-Case-Study-20190710.pdf" download>{button_text}</a>
-<p>
-{description} <span>{boldtext}</span>
-</p>
-</div>
-</div>
-    )
-} 
-
+export default function Case_study({
+  title,
+  button_text,
+  description,
+  boldtext,
+}) {
+  return (
+    <div className="Case_study">
+      <div className="content">
+        <h3>{title}</h3>
+        <a
+          download
+          target="_blank"
+          href="../assets/Anatomie-Metrical-Case-Study-20190710.pdf"
+        >
+          {button_text}
+        </a>
+        <p>
+          {description} <span>{boldtext}</span>
+        </p>
+      </div>
+    </div>
+  )
+}
